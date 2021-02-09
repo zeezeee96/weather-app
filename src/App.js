a8e72a607c49d89b57bf2084ab43982f49d5a079
zeezeee96 <76630399+zeezeee96@users.noreply.github.com>
2021-02-09 20:02:29 +0500

    Added Weather Detail Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Weatherdetails from './components/Weatherdetails';
+import Fetchapi from './components/fetchapi';
 
-function App() {
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/weatherdetails">Weatherdetails</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+        <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/weatherdetails">
+            <Weatherdetails />
+            <Fetchapi />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
+function Home() {
+  return <h2>This is the routing app. Switching two apps</h2>;
+}
 export default App;
