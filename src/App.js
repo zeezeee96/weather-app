35656a5d9ff4242f0f90371ab4846438852e30ab
zeezeee96 <76630399+zeezeee96@users.noreply.github.com>
2021-02-12 08:26:09 +0500

    Created Searchbar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Weatherdetails from './components/Weatherdetails';
+import Fetchapi from './components/fetchapi';
 
-function App() {
+ function App() {
+   const [weatherdata, setWeatherData]=useState(""); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+        <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/weatherdetails">
+            <Weatherdetails
+            weatherdata={weatherdata}
+            />
+            <Fetchapi 
+            onDetailsClick={(x)=>setWeatherData(x)}/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
