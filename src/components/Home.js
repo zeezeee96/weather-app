e7f5a38d0b4f59d75c0c13c7cf4dac7e9771c8b9
zeezeee96 <76630399+zeezeee96@users.noreply.github.com>
2021-02-19 08:24:47 +0500

    Added Top Cities Section
    Added five top cities which shows current temperature of those 5 cities.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,55 @@
-import React, { useEffect, useState } from "react";
-import { fetchApi } from "../services/request-service";
+import React, { useEffect, useState, useStyles } from "react";
+import { searchCities, getTopCities } from "../services/request-service";
 import { useHistory } from "react-router-dom";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardActionArea from '@material-ui/core/CardActionArea';
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [city, setCity] = useState("");
   const [options, setOptions] = useState([]);
-
+  const [cities, setCities] = useState([]);
   let history = useHistory();
+
+  const useStyles = makeStyles({
+    root: {
+      maxWidth: 200,
+    },
+    bullet: {
+      display: "inline-block",
+      margin: "0 2px",
+      transform: "scale(0.8)",
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+  const classes = useStyles();
+
+  useEffect(() => {
+    getTopCities().then((res) => {
+      const topfive = res.slice(0, 5);
+      setCities(topfive);
+    });
+  }, []);
 
   const search = (newInputValue) => {
     setQuery(newInputValue);
-    fetchApi(newInputValue).then((data) => setOptions(data));
+    searchCities(newInputValue).then((data) => setOptions(data));
   };
 
   return (
     <div>
       <Autocomplete
         id="weather-search"
-        value={city}
         onChange={(event, newValue) => {
-          setCity(newValue);
           sessionStorage.setItem("cityname", JSON.stringify(newValue));
           history.push("/weatherdetails");
         }}
@@ -40,6 +67,30 @@ const Home = () => {
           <TextField {...params} label="Search City Name" variant="outlined" />
         )}
       />
+
+      <div>
+        {cities.map((x) => {
+          console.log(x);
+          return (
+            <Card key={x.Key}className={classes.root}>
+              <CardActionArea>
+              <CardContent>
+                <Typography
+                  className={classes.title}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  {x.EnglishName}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {x.Temperature.Metric.Value }<sup>&deg;</sup>C
+                </Typography>
+              </CardContent>
+              </CardActionArea>
+            </Card>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/services/request-service.js
+++ src/services/request-service.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { API_BASE_URL,API_AUTOCOMPLETE_URL, API_KEY } from "../constants/constants";
+import { API_BASE_URL,API_AUTOCOMPLETE_URL, API_KEY, API_TOPCITIES_URL } from "../constants/constants";
 
-export const fetchApi = async (query) => {
-  const { data } = await axios.get(API_BASE_URL+API_AUTOCOMPLETE_URL, {
-    params: {
-      q: query,
-      apikey: API_KEY,
-    },
+export const searchCities =  (query) => {
+  return  new Promise ((resolve,reject) => {
+    axios.get(API_BASE_URL+API_AUTOCOMPLETE_URL, {
+      params: {
+        q: query,
+        apikey: API_KEY,
+      },
+  }).then((response) => resolve(response.data))
   });
-  return data;
+};
+export const getTopCities =  () => {
+  return new Promise ((resolve,reject)=> {
+    axios.get(API_BASE_URL+API_TOPCITIES_URL, {
+      params: {
+        apikey: API_KEY,
+      }}).then((response) => resolve(response.data))
+  })
 };
 
